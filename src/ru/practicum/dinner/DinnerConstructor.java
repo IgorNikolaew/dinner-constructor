a8da80041e7c0f6f8cd2f9ec50292fe04114bfe1
Igor Arkadievich <igor@yandex.ru>
2025-10-07 19:16:35 +0300

    Мой коммит в библиотеке

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -3,7 +3,7 @@ package ru.practicum.dinner;
 import java.util.ArrayList;
 
 import java.util.HashMap;
-import java.util.Random; //    ПОКА НЕ ЗНАЮ ЧТО ТУТ
+import java.util.Random; //    ПОКА НЕ ЗНАЮ ЧТО ТУТ т
 
 public class DinnerConstructor {
 
